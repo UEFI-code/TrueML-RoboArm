a8775a8c98160bf16a5799f6707e3f6ff3cc1d46
UEFI <1092700291@qq.com>
2022-05-20 10:42:28 +0000

    Update code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ServoMotorNum = 4
 
 class SuperServos():
-    def __init__(self, nums) -> None:
+    def __init__(self, nums, delta) -> None:
         self.ServoAngles = []
         self.ServoExperResultsP = []
         self.ServoExperResultsN = []
@@ -9,6 +9,8 @@ class SuperServos():
             self.ServoAngles.append(45.0)
             self.ServoExperResultsP.append(0.0)
             self.ServoExperResultsN.append(0.0)
+        self.ServoNum = nums
+        self.delta = delta
     
     def Move(self, id, action):
         if self.ServoAngles[id] + action > 0 and self.ServoAngles[id] + action < 180:
@@ -16,3 +18,29 @@ class SuperServos():
             return True
         else:
             return False
+    
+    def Measure(self):
+        return [1, 2, 3]
+
+    def ComputeEffect(self, Before, After):
+        s = 0
+        for i, j in zip(Before, After):
+            s += (i - j) * (i - j)
+        return s
+
+    def Experiment(self):
+        for id in range(self.ServoNum):
+            self.ServoExperResultsP[id] = 0.0
+            self.ServoExperResultsN[id] = 0.0
+            statusBefore = self.Measure()
+            res = self.Move(id, self.delta)
+            if(res):
+                statusAfter = self.Measure()
+                self.ServoExperResultsP[id] = self.ComputeEffect(statusBefore, statusAfter)
+                self.Move(id, 0 - self.delta)
+                statusBefore = self.Measure()
+            res = self.Move(id, 0 - self.delta)
+            if(res):
+                statusAfter = self.Measure()
+                self.ServoExperResultsN[id] = self.ComputeEffect(statusBefore, statusAfter)
+                self.Move(id, self.delta)
