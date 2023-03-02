d502f7f0e36a30beda402ef9b8bc29aec67967d4
UEFI-code <1092700291@qq.com>
2023-03-02 00:04:39 +0000

    Add Evaluation Code

--- train_virt.py
+++ train_virt.py
@@ -41,15 +41,26 @@ def getMiniBatch(batchSize, motors, servoObj):
         outputX.append(motorData)
         outputY.append(realPos)
     return torch.tensor(outputX, dtype=torch.float), torch.tensor(outputY, dtype=torch.float)
+
+def getVirtExperimentResult(myServoObj, batch):
+    outputY = []
+    for i in batch:
+        motorData = i.cpu().detach() * 180
+        myServoObj.servoAngles = motorData
+        realPos = myServoObj.calc3DPos()
+        realPos = np.array(realPos)
+        realPos = realPos / sum(myServoObj.armLength)
+        outputY.append(realPos)
+    return torch.tensor(outputY, dtype=torch.float)
     
 def trainPredictor(batchSize, motors, servoObj, predictor, optimizer, lossFunc, epochs):
     for epoch in range(epochs):
+        optimizer.zero_grad()
         # get data
         print("Getting data...")
         x, y = getMiniBatch(batchSize, motors, servoObj)
         # train
         print("Training...thePredictor")
-        optimizer.zero_grad()
         output = predictor(x)
         loss = lossFunc(output, y)
         loss.backward()
@@ -59,14 +70,14 @@ def trainPredictor(batchSize, motors, servoObj, predictor, optimizer, lossFunc, 
 
 def trainDecider(batchSize, motors, servoObj, decider, optimizer, lossFunc, epochs):
     for epoch in range(epochs):
+        optimizer.zero_grad()
         # get data
         print("Getting data...")
         x, y = getMiniBatch(batchSize, motors, servoObj)
         # train
         print("Training...theDecider")
-        optimizer.zero_grad()
-        output = decider(y)
-        loss = lossFunc(output, x)
+        action = decider(y)
+        loss = lossFunc(action, x)
         loss.backward()
         optimizer.step()
         # print info
@@ -74,10 +85,10 @@ def trainDecider(batchSize, motors, servoObj, decider, optimizer, lossFunc, epoc
 
 def teachDecider(batchSize, predictor, decider, optimizerDecider, lossFunc, epochs):
     for epoch in range(epochs):
+        optimizerDecider.zero_grad()
         # generate dummy targets
         print("Generating dummy targets...")
         targets = torch.rand(batchSize, 3)
-        optimizerDecider.zero_grad()
         kasoX = decider(targets)
         kasoY = predictor(kasoX)
         # We belive predictor more strongly than decider
@@ -87,9 +98,24 @@ def teachDecider(batchSize, predictor, decider, optimizerDecider, lossFunc, epoc
         # print info
         print("The Decider teach Epoch: " + str(epoch) + " | Loss: " + str(loss.item()))
 
+def testPredictor(batchSize, motors, servoObj, predictor):
+    x, y = getMiniBatch(batchSize, motors, servoObj)
+    output = predictor(x)
+    simliar = nn.CosineSimilarity(dim=1, eps=1e-6)(output, y)
+    print("The Predictor test result: " + str(simliar.mean().item() * 100) + "%")
+
+def testDecider(batchSize, servoObj, decider):
+    y = torch.rand(batchSize, 3)
+    action = decider(y)
+    target = getVirtExperimentResult(servoObj, action)
+    simliar = nn.CosineSimilarity(dim=1, eps=1e-6)(target, y)
+    print("The Decider test result: " + str(simliar.mean().item() * 100) + "%")
+
 trainPredictor(BatchSize, Motors, myServoDrv, thePredictor, optimPredictor, lossFunc, Epochs)
 torch.save(thePredictor.state_dict(), "thePredictor.pth")
-trainDecider(BatchSize, Motors, myServoDrv, theDecider, optimDecider, lossFunc, Epochs * 10)
+trainDecider(BatchSize, Motors, myServoDrv, theDecider, optimDecider, lossFunc, Epochs * 5)
 torch.save(theDecider.state_dict(), "theDecider.pth")
 # teachDecider(BatchSize, thePredictor, theDecider, optimDecider, lossFunc, Epochs)
 # torch.save(theDecider.state_dict(), "theDecider-finetuned.pth")
+testPredictor(BatchSize, Motors, myServoDrv, thePredictor)
+testDecider(BatchSize, myServoDrv, theDecider)
