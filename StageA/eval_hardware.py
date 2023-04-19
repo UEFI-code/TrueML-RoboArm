d765efaf06917bbb211120ed3f06ab84eda11af4
UEFI-code <zhuoshizhang@hotmail.com>
2023-04-19 19:20:19 +0800

    Improve Normalization

--- StageA/eval_hardware.py
+++ StageA/eval_hardware.py
@@ -22,9 +22,9 @@ virtualArm = virtualKinematic.theVirtualArm()
 
 while True:
     x,y,z = input('Please input the goal: ').split()
-    x = float(x)
-    y = float(y)
-    z = float(z)
+    x = float(x) / 4
+    y = float(y) / 4
+    z = float(z) / 4
     goal = torch.tensor([x,y,z]).cuda()
     goal.unsqueeze_(0)
     action = decider(goal)
